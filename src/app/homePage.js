9afb836652ba97d5c7de7563060ed2f6abdb7ccc
Jaryd Hibbert <jarydhibbert@gmail.com>
2024-12-09 22:27:12 -0700

    did some stuff and things

---
+++ src/app/homePage.js
@@ -0,0 +1,13 @@
+import Header from "./header";
+import Footer from "./footer";
+
+export default function HomePage() {
+  return ( 
+    <div>  
+      <title>PokeCollector</title> 
+      <h1>PokeCollector</h1>
+
+    
+    </div>
+  );
+}
